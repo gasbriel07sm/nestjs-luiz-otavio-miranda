ed06ef4305cc31bf785cd7cf5332564f45496ec8
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-01 13:50:42 -0300

    services

--- conceitos_nest/src/conceitos-manual/conceitos-manual.module.ts
+++ conceitos_nest/src/conceitos-manual/conceitos-manual.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConceitosManualController } from './conceitos-manual.controller';
+import { ConceitosManualService } from './conceitos-manual.service';
 
 @Module({
+  providers: [ConceitosManualService],
   controllers: [ConceitosManualController],
 })
 export class ConceitosManualModule {}
