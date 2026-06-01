d6de09b68d98b2e51408c9911662777446c05e10
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-01 13:32:11 -0300

    controllers

--- conceitos_nest/src/app/app.controller.ts
+++ conceitos_nest/src/app/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('home')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello') // Método da solicitação -> Ler (Read)
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('exemplo')
+  exemplo(): string {
+    return 'Exemplo de rota';
   }
 }
