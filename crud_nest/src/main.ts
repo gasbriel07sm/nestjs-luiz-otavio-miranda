bdca474d3fe6b1851cb54565e6dab5a32f2e33b7
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-02 14:56:24 -0300

    segurança com whitelist e forbidNonWhitelisted

--- crud_nest/src/main.ts
+++ crud_nest/src/main.ts
@@ -1,10 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove chaves que não estão no DTO
+      forbidNonWhitelisted: true, // retorna erro se tiver chaves que não estão no DTO
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
