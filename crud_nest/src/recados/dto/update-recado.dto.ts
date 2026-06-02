8328754a578274dda44f901523cef82bcbc241ec
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-02 14:49:41 -0300

    usando PartialType para validação de campos

--- crud_nest/src/recados/dto/update-recado.dto.ts
+++ crud_nest/src/recados/dto/update-recado.dto.ts
@@ -1,5 +1,17 @@
-export class UpdateRecadoDto {
-  readonly texto?: string;
-  readonly de?: string;
-  readonly para?: string;
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { CreateRecadoDto } from './create-recado.dto';
+
+export class UpdateRecadoDto extends PartialType(CreateRecadoDto) {
+  // podemos sobreescrever campos
+  // @IsNotEmpty()
+  // @IsString()
+  // @MinLength(5)
+  // @MaxLength(255)
+  // readonly texto!: string;
+
+  @IsBoolean()
+  @IsOptional()
+  readonly lido?: boolean;
 }
--- crud_nest/src/recados/recados.service.ts
+++ crud_nest/src/recados/recados.service.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Recado } from './entities/recado.entity';
 import { CreateRecadoDto } from './dto/create-recado.dto';
