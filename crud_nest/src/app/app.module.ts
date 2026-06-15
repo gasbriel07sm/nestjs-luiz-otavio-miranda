88776b5844b62f6b517efe8dbb402e84c81783bd
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-06-15 16:45:31 -0300

    primeira entity do TypeOrm

--- crud_nest/src/app/app.module.ts
+++ crud_nest/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RecadosModule } from '../recados/recados.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RecadosModule } from '../recados/recados.module';
 
 @Module({
-  imports: [RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: '123',
+      autoLoadEntities: true, // Carrega entidades sem precisar especificá-las
+      synchronize: true, // Sincroniza com o Banco de Dados, não deve ser usado em produção
+    }),
+    RecadosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
